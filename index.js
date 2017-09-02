e13b69550f8ab70fd0a19906c40ce7d4162e0ab5
Anshuman Bora <anshumanbora04@gmail.com>
2017-09-02 01:12:37 -0700

    server created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send({hi:'there'});
+});
+
+/*---Dynamic port binding done here----
+    Use 5000 port in development otherwise
+    let Heroku decide dynamically
+*/
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
