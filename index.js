72f0233e4165c6faa868e41539d00a09cb8b534e
Anshuman Bora <anshumanbora04@gmail.com>
2017-09-02 01:38:00 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-  res.send({hi:'there'});
+  res.send({hi:'Hola amigo'});
 });
 
 /*---Dynamic port binding done here----
